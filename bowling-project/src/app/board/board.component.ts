7a19147e4df2570216ad3f4d9a6703a029fafdc6
reid <36016470+srh09@users.noreply.github.com>
2020-01-10 13:41:00 -0600

    styling, basic validation

--- bowling-project/src/app/board/board.component.ts
+++ bowling-project/src/app/board/board.component.ts
@@ -9,7 +9,7 @@ import { ScoringService } from '../scoring.service';
 })
 export class BoardComponent {
   
-  f1o1: number = 0;
+  f1o1: number = null;
   f1o2: number = null;
   f2o1: number = null;
   f2o2: number = null;
@@ -32,27 +32,48 @@ export class BoardComponent {
   f10o3: number = null;
 
   sum = null;
-  testRoll: number[] = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
 
   options: Option[] = [
-    {value: 0, viewValue: '0'},
-    {value: 1, viewValue: '1'},
-    {value: 2, viewValue: '2'},
-    {value: 3, viewValue: '3'},
-    {value: 4, viewValue: '4'},
-    {value: 5, viewValue: '5'},
-    {value: 6, viewValue: '6'},
-    {value: 7, viewValue: '7'},
-    {value: 8, viewValue: '8'},
-    {value: 9, viewValue: '9'},
-    {value: 10, viewValue: 'X'}
+    {value: 0, viewValue: '0 Pins'},
+    {value: 1, viewValue: '1 Pin'},
+    {value: 2, viewValue: '2 Pins'},
+    {value: 3, viewValue: '3 Pins'},
+    {value: 4, viewValue: '4 Pins'},
+    {value: 5, viewValue: '5 Pins'},
+    {value: 6, viewValue: '6 Pins'},
+    {value: 7, viewValue: '7 Pins'},
+    {value: 8, viewValue: '8 Pins'},
+    {value: 9, viewValue: '9 Pins'},
+    {value: 10, viewValue: '10 Pins'},
+    {value: null, viewValue: 'null'}
   ];
 
   constructor(private scoringService: ScoringService) { }
 
   calculateClick() {
-    var rollArray: number[] = [this.f1o1];
-    this.sum = this.scoringService.getScore(this.testRoll);
+    var rollArray: number[] = [
+      this.f1o1, this.f1o2, this.f2o1, this.f2o2, this.f3o1, this.f3o2, this.f4o1, this.f4o2, this.f5o1, this.f5o2, this.f6o1, this.f6o2,
+      this.f7o1, this.f7o2, this.f8o1, this.f8o2, this.f9o1, this.f9o2, this.f10o1, this.f10o2, this.f10o3
+    ];
+    rollArray = rollArray.filter(function (el) {return el != null;});
+    this.sum = this.scoringService.getScore(rollArray);
+  }
 
+  clearClick() {
+    this.f1o1 = null, this.f1o2 = null, this.f2o1 = null, this.f2o2 = null, this.f3o1 = null, this.f3o2 = null,
+    this.f4o1 = null, this.f4o2 = null, this.f5o1 = null, this.f5o2 = null, this.f6o1 = null, this.f6o2 = null,
+    this.f7o1 = null, this.f7o2 = null, this.f8o1 = null, this.f8o2 = null, this.f9o1 = null, this.f9o2 = null,
+    this.f10o1 = null, this.f10o2 = null, this.f10o3 = null, this.sum = null
+  }
+
+  validationCheck(rollOne: number) {
+    if (rollOne == null || rollOne == 10) {
+      return false;
+    }
+    return true;
+  }
+
+  isNaN(evaluatedNumber: number) {
+    return Number.isNaN(evaluatedNumber);
   }
 }
